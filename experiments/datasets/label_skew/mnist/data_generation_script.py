b5db7fdac1704023a872cef06bac658f9b09114b
Scolpe <maciejzuziak101@gmail.com>
2024-06-17 13:08:33 +0200

    Establishes scripts for experiments

---
+++ experiments/datasets/label_skew/mnist/data_generation_script.py
@@ -0,0 +1,37 @@
+from fedata.hub.generate_dataset import generate_dataset
+import os
+
+def main():
+    data_config = {
+    "dataset_name" : "mnist",
+    "split_type" : "dirchlet_clusters_nooverlap",
+    "shards": 12,
+    "local_test_size": 0.3,
+    "transformations": {},
+    "imbalanced_clients": {},
+    "save_dataset": True,
+    "save_transformations": True,
+    "save_blueprint": True,
+    "agents": 12,
+    "shuffle": True,
+    "alpha": 1,
+    "allow_replace": True,
+    "save_path": os.getcwd(),
+    "agents_cluster_belonging":
+        {
+            1: [0, 1, 2, 3],
+            2: [4, 5, 6, 7],
+            3: [8, 9, 10, 11]
+        },
+    "missing_classes":
+        {
+            1: [0, 1, 2],
+            2: [3, 4, 5],
+            3: [6, 7, 8, 9]
+        }
+    }
+    generate_dataset(config=data_config)
+
+
+if __name__ == "__main__":
+    main()
---
+++ experiments/datasets/quality_skew/fmnist/data_generation_script.py
@@ -0,0 +1,31 @@
+from fedata.hub.generate_dataset import generate_dataset
+import os
+
+def main():
+    data_config = {
+    "dataset_name" : "fmnist",
+    "split_type" : "homogeneous",
+    "shards": 12,
+    "local_test_size": 0.3,
+    "transformations": {0: {"transformation_type": "noise", "noise_multiplyer": 0.01},
+                        1: {"transformation_type": "noise", "noise_multiplyer": 0.05},
+                        2: {"transformation_type": "noise", "noise_multiplyer": 0.15},
+                        3: {"transformation_type": "noise", "noise_multiplyer": 0.5},
+                        4: {"transformation_type": "noise", "noise_multiplyer": 1.0},
+                        5: {"transformation_type": "rotation", "degrees": 270},
+                        6: {"transformation_type": "rotation", "degrees": 270},
+                        7: {"transformation_type": "perspective_change", "distortion_scale": 0.9, "transformation_probability": 0.9},
+                        8: {"transformation_type": "perspective_change", "distortion_scale": 0.9, "transformation_probability": 0.9}
+                        },
+    "imbalanced_clients": {},
+    "save_dataset": True,
+    "save_transformations": True,
+    "save_blueprint": True,
+    "agents": 10,
+    "shuffle": True,
+    "save_path": os.getcwd()}
+    generate_dataset(config=data_config)
+
+
+if __name__ == "__main__":
+    main()
