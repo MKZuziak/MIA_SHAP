0da8a1a50699fd8d6eb86770e2334ca5414775f8
Scolpe <maciejzuziak101@gmail.com>
2024-09-03 14:11:01 +0200

    New simulation scripts for Stage II

---
+++ experiments/stage_II_without_dp/lightly_skewed/cifar10/simulation_script.py
@@ -0,0 +1,67 @@
+import os
+from functools import partial
+import pickle
+
+from torch import optim
+
+from tests.test_props.datasets import return_mnist
+from tests.test_props.nets import NeuralNetwork
+from SHAP_MIA.model.federated_model import FederatedModel
+from SHAP_MIA.node.federated_node import FederatedNode
+from SHAP_MIA.simulation.simulation import Simulation
+from SHAP_MIA.aggregators.fedopt_aggregator import Fedopt_Optimizer
+from SHAP_MIA.files.archive import create_archive
+
+DATASET_PATH = None
+NET_ARCHITECTURE = None
+NUMBER_OF_CLIENTS = 8
+ITERATIONS = 80
+LOCAL_EPOCHS = 3
+LOADER_BATCH_SIZE = 64
+LEARNING_RATE = 0.001
+ARCHIVE_PATH = None
+ARCHIVE_NAME = None
+
+
+def integration_test():
+    (metrics_savepath, 
+     nodes_models_savepath, 
+     orchestrator_model_savepath) = create_archive(
+         path=ARCHIVE_PATH,
+         archive_name=ARCHIVE_NAME
+     )
+    
+    with open(DATASET_PATH, 'rb') as file:
+        data = pickle.load(file)
+    orchestrators_data = data[0]
+    nodes_data = data[1]
+    net_architecture = NET_ARCHITECTURE
+    optimizer_architecture = partial(optim.SGD, lr=LEARNING_RATE)
+    model_tempate = FederatedModel(
+        net=net_architecture,
+        optimizer_template=optimizer_architecture,
+        loader_batch_size=LOADER_BATCH_SIZE
+    )
+    node_template = FederatedNode()
+    fed_avg_aggregator = Fedopt_Optimizer()
+    
+    simulation_instace = Simulation(model_template=model_tempate,
+                                    node_template=node_template)
+    simulation_instace.attach_orchestrator_model(orchestrator_data=orchestrators_data)
+    simulation_instace.attach_node_model({
+            node: nodes_data[node] for node in range(NUMBER_OF_CLIENTS)
+        })
+    simulation_instace.training_protocol(
+        iterations=ITERATIONS,
+        sample_size=NUMBER_OF_CLIENTS,
+        local_epochs=LOCAL_EPOCHS,
+        aggrgator=fed_avg_aggregator,
+        shapley_processing_batch=NUMBER_OF_CLIENTS,
+        metrics_savepath=metrics_savepath,
+        nodes_models_savepath=nodes_models_savepath,
+        orchestrator_models_savepath=orchestrator_model_savepath
+    )
+
+
+if __name__ == "__main__":
+    integration_test()
