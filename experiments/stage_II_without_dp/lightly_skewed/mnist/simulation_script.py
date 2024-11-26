fb4ba210d20371365791f03aa08b87eeecf28e4b
MKZuziak <maciejzuziak101@gmail.com>
2024-11-26 13:33:04 +0100

    STAGE_II/Lightly_Skewed/MNIST + FMNIST

--- experiments/stage_II_without_dp/lightly_skewed/mnist/simulation_script.py
+++ experiments/stage_II_without_dp/lightly_skewed/mnist/simulation_script.py
@@ -2,18 +2,20 @@ import os
 from functools import partial
 import pickle
 
-from torch import optim
-
 import timm
 
+from torch import optim
+
+from tests.test_props.datasets import return_mnist
+from tests.test_props.nets import NeuralNetwork
 from SHAP_MIA.model.federated_model import FederatedModel
 from SHAP_MIA.node.federated_node import FederatedNode
 from SHAP_MIA.simulation.simulation import Simulation
 from SHAP_MIA.aggregators.fedopt_aggregator import Fedopt_Optimizer
 from SHAP_MIA.files.archive import create_archive
 
-DATASET_PATH = r'/home/maciejzuziak/raid/MIA_SHAP/experiments/datasets/uniform/mnist/MNIST_8_dataset_pointers'
-NET_ARCHITECTURE = timm.create_model('mobilenetv3_small_100', num_classes=10, pretrained=False, in_chans=1)
+DATASET_PATH = r'/home/maciejzuziak/raid/MIA_SHAP/experiments/datasets/lightly_skewed/mnist/MNIST_8_dataset_pointers'
+NET_ARCHITECTURE = timm.create_model('resnet18', num_classes=10, pretrained=False, in_chans=1)
 NUMBER_OF_CLIENTS = 8
 ITERATIONS = 40
 LOCAL_EPOCHS = 2
@@ -21,7 +23,6 @@ LOADER_BATCH_SIZE = 32
 LEARNING_RATE = 0.001
 ARCHIVE_PATH = os.getcwd()
 ARCHIVE_NAME = 'withoutDP_uniform_mnist'
-
 
 def integration_test():
     (metrics_savepath, 
@@ -35,7 +36,6 @@ def integration_test():
         data = pickle.load(file)
     orchestrators_data = data[0]
     nodes_data = data[1]
-    print(nodes_data)
     net_architecture = NET_ARCHITECTURE
     optimizer_architecture = partial(optim.SGD, lr=LEARNING_RATE)
     model_tempate = FederatedModel(
